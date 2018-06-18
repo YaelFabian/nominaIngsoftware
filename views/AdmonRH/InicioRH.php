fb552f97f1fc2286b423932604ed317e3205a13e
YaelFabian <yael_fabian@hotmail.com>
2018-06-17 22:49:43 -0500

    Update InicioRH.php

--- views/AdmonRH/InicioRH.php
+++ views/AdmonRH/InicioRH.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+define( 'MAX_SESSION_TIEMPO', 900 * 1 );
 if(!$_SESSION["EmpleadoRH"]){
 
 	header("location:../../1SelecciónEA.php");
@@ -8,6 +8,15 @@ if(!$_SESSION["EmpleadoRH"]){
 	exit();
 
 }else{
+	if ( isset( $_SESSION[ 'ULTIMA_ACTIVIDAD' ] ) && 
+     ( time() - $_SESSION[ 'ULTIMA_ACTIVIDAD' ] > MAX_SESSION_TIEMPO ) ) {
+
+    // Si ha pasado el tiempo sobre el limite destruye la session
+    header("location:../../cerrar.php");
+
+	exit();
+    }
+$_SESSION[ 'ULTIMA_ACTIVIDAD' ] = time();
 require_once ("../../models/enlacesRH.php");
 require_once ("../../models/CrudEmpleado.php");
 require_once ("../../models/CrudConcepto.php");
